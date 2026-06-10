0d6b4a4f6e79dfd7948c739745deb019d26c8228
GanbaruKing <kawaguchikubota@gmail.com>
2026-06-11 05:17:12 +0900

    fix bug

--- BloodMazeCode/Cards/Uncommon/Agony.cs
+++ BloodMazeCode/Cards/Uncommon/Agony.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using BaseLib.Utils;
 using BloodMaze.BloodMazeCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
@@ -12,19 +15,19 @@ using MegaCrit.Sts2.Core.ValueProps;
 
 namespace BloodMaze.BloodMazeCode.Cards.Uncommon;
 
- 
+
 public class Agony() : MpConsumeCard(1, CardType.Attack,
     CardRarity.Uncommon, TargetType.AnyEnemy, 2)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
         ..base.CanonicalVars,
         new CalculationBaseVar(8m),
-        new ExtraDamageVar(0m), 
-        
-        
+        new ExtraDamageVar(1m), 
         new CalculatedDamageVar(ValueProp.Move).WithMultiplier(
             ((Func<CardModel, Creature, decimal>)((card, target) =>
             {
+
                 if (target == null) return 1m;
                 return (decimal)target.GetPowerAmount<HemorrhagePower>();
             })!)!)
@@ -32,17 +35,26 @@ public class Agony() : MpConsumeCard(1, CardType.Attack,
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
+        ArgumentNullException.ThrowIfNull(play.Target, "play.Target");
+
         if (IsVampireForm)
         {
+
             await CreatureCmd.Damage(choiceContext, Owner.Creature, MpCost,
                 ValueProp.Unblockable | ValueProp.Unpowered, this);
-            await VampireAttack(choiceContext, play.Target);
+
+            AttackCommand attack = await CommonActions
+                .CardAttack(this, play.Target, DynamicVars.CalculatedDamage)
+                .Execute(choiceContext);
+
+            decimal restore = attack.Results.Sum(r => r.TotalDamage + r.OverkillDamage);
+            await CreatureCmd.Heal(this.Owner.Creature, restore);
+
             IsVampireForm = false;
             return;
         }
 
         ConsumeMp();
-        ArgumentNullException.ThrowIfNull(play.Target, "play.Target");
         await DamageCmd.Attack(DynamicVars.CalculatedDamage)
             .FromCard(this)
             .Targeting(play.Target)
