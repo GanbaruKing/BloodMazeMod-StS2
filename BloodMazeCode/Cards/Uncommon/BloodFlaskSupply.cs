b094faa63474cad9f8f0a8b15917b6efd737765f
GanbaruKing <kawaguchikubota@gmail.com>
2026-06-18 03:06:29 +0900

    fix merchant freeze bug

--- BloodMazeCode/Cards/Uncommon/BloodFlaskSupply.cs
+++ BloodMazeCode/Cards/Uncommon/BloodFlaskSupply.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using BaseLib.Utils;
+using BloodMaze.BloodMazeCode.Cards.Token;
 using BloodMaze.BloodMazeCode.Powers;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 
 namespace BloodMaze.BloodMazeCode.Cards.Uncommon;
@@ -14,6 +16,7 @@ public class BloodFlaskSupply() : BloodMazeCard(0,
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<BloodFlaskSupplyPower>(0m)];
 
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<BloodFlask>(this.IsUpgraded)];
     public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
 
     protected override bool HasEnergyCostX => true;
