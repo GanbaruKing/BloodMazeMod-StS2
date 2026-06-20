da046b7f1591daf621209f9ca27572866b0525f2
GanbaruKing <kawaguchikubota@gmail.com>
2026-06-20 20:01:43 +0900

    fix localaizations

--- BloodMazeCode/Cards/Uncommon/Innovation.cs
+++ BloodMazeCode/Cards/Uncommon/Innovation.cs
@@ -4,6 +4,7 @@ using BloodMaze.BloodMazeCode.Cards.Token;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
@@ -16,6 +17,7 @@ public class Innovation() : MpConsumeCard(1,
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3), ..base.CanonicalVars, new HpLossVar(3m)];
 
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<BloodFlask>()];
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
